b4185399820a474f8d90ea77a567ed7ea9c43c3f
Huiren Woo <giantcrabby@gmail.com>
2021-09-08 21:56:30 +0800

    Add files

---
+++ README.md
@@ -0,0 +1,21 @@
+# HSG Label Printer
+
+You need the drivers!
+
+* install dymo-cups-drivers
+* configure printer:
+* Select from Database
+* -> DYMO
+* ---> LabelWriter 4XL
+
+## How it works
+HTML canvas -> ExpressJS -> converts image url to pdf -> print!
+
+## Usage
+
+```
+npm install
+npm start
+```
+
+now server runs on port 3000
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+const ipp = require('ipp');
+const fs = require("fs");
+const jspdf = require("jspdf");
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+router.post('/print', function (req, res, next) {
+    const endpoint = 'http://localhost:631/printers/LabelWriter-4XL';
+    const printer = ipp.Printer(endpoint);
+
+    const imgData = req.body['input-data'];
+
+    const pdf = new jspdf.jsPDF();
+
+    pdf.addImage({imageData: imgData, x: 0, y: 0, width: 104, height: 159});
+    pdf.save("download.pdf");
+
+
+    fs.readFile("download.pdf", function (err, data) {
+        const msg = {
+            "operation-attributes-tag": {
+                "requesting-user-name": "woohuiren",
+                "document-format": "application/pdf"
+            },
+            "job-attributes-tag": {
+                "sides": "one-sided",
+            },
+            data: data
+        };
+
+        printer.execute("Print-Job", msg, function (err, printerRes) {
+            if (err) {
+                res.json({error: err});
+            } else {
+                res.json({message: printerRes});
+            }
+        });
+    });
+});
+
+
+module.exports = router;
